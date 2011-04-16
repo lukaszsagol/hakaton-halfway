726710924b224fe6b9704d8d5790e0412a833185
Łukasz Sągol <lukasz@sagol.pl>
2011-04-16 13:01:23 +0200

    map

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,5 +1,6 @@
+var map = null
 jQuery(function($) {
-    var map = new google.maps.Map($('#map_canvas')[0], {
+    map = new google.maps.Map($('#map_canvas')[0], {
       zoom: 17,
       center: new google.maps.LatLng(52.219505, 21.012436),
       mapTypeId: google.maps.MapTypeId.ROADMAP
