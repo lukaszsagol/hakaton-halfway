87e4c7c441ab741852ce54c29884f5b39f73b5cc
Michal Szyndel <michal@wyklad-108.elka.pw.edu.pl>
2011-04-16 12:08:05 +0200

    fetching places

--- javascripts/fb.js
+++ javascripts/fb.js
@@ -1,1 +1,23 @@
 # empty
+
+var fbPlaces = function(args) {
+	
+	var defaults = {
+		distance: 1000,
+	}
+	var opts = $.extend({}, defaults, args);
+	
+	var placesUrl = 'https://graph.facebook.com/search?q=%query&type=place&center=%lat,%lon&distance=%distance&access_token=%token&callback=';
+	
+	var search(lat, lon, query) {
+		url = placesUrl.replace('%query', query)
+						.replace('%lat', lat)
+						.replace('%lon', lon)
+						.replace('%distance', opts.distance)
+						.replace('%token', opts.token);
+		$.getJSON(url, function(data){
+			console.log(data);
+		});
+	}
+	
+}
